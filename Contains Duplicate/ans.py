17ae7eb106b0ff38e7675c655c1985ead4c83ed5
j harishankar <jharivichu27@gmail.com>
2025-07-27 10:05:48 +0530

    two pointer -5

---
+++ Contains Duplicate/ans.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+    
+        if len(set(nums))==len(nums):
+            return False
+        else:
+            return True
+
+            
+            
+
+            
