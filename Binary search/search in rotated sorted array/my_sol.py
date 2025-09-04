2ffd86d8576393d67e9a89c1cb5fbe02d2368046
j harishankar <jharivichu27@gmail.com>
2025-09-04 22:26:47 +0530

    binary search -medium

---
+++ Binary search/search in rotated sorted array/my_sol.py
@@ -0,0 +1,37 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        pivot = self.pivot_element(nums)
+        if len(nums) == 1:
+            if nums[0] == target:
+                return 0
+            else:
+                return -1
+        left = self.helper(nums,target,0,pivot-1)
+        right = self.helper(nums,target,pivot,len(nums)-1)
+        return max(left,right)
+
+    def pivot_element(self,nums):
+        l=0
+        r=len(nums)-1
+        while l<=r:
+            mid = (l+r) // 2
+            if nums[l] <= nums[r]:
+                return 0 
+            if nums[mid] < nums[mid-1]:
+                return mid
+            if nums[mid] > nums[mid + 1]:
+                return mid + 1
+            elif nums[mid] < nums[l]:
+                r = mid -1 
+            elif nums[mid] > nums[l]:
+                l = mid + 1
+    def helper(self,nums,target,l,r):
+        while l<=r:
+            mid = (l+r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
